a9797d3acf9bd552676c8a5d7cac1fa688798293
Gastech99 <diabygassamad16@gmail.com>
2025-01-13 15:22:33 +0000

    Révision des approches itératives et récursives en Java

--- src/parts/Functions.java
+++ src/parts/Functions.java
@@ -9,9 +9,25 @@ public class Functions {
         return val1 + val1;
     }
 
+    // Approche itérative
+    public int factoriel_iterative(int n){
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // Approche récursive
+    public int factoriel_recursive(int n){
+        if (n == 0) return 1;
+        else return factoriel_recursive(n-1)*n;
+    }
     public static void main(String[] args){
         Functions test = new Functions();
-        System.out.println(test.function1());
-        System.out.println(test.function2(5, 9));
+        System.out.println("Résultat de la fonction 1 = "+test.function1());
+        System.out.println("Résultat de la fonction 2 = "+test.function2(5, 9));
+        System.out.println("L'approche itérative = "+test.factoriel_iterative(5));
+        System.out.println("L'approche récursive = "+test.factoriel_recursive(5));
     }
 }
